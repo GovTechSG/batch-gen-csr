d86cb802c1c3365d46d364a2dd9d74e34fbe34ac
herjean <wong0786@e.ntu.edu.sg>
2018-10-05 07:00:04 +0000

    Second batch file to run

---
+++ GenerateKeyStore.bat
@@ -0,0 +1,22 @@
+#File name has to be the same as the one previously defined
+SET fileName=myFirstL2Cert
+
+#Navigate to SSL exe folder. Amend the path accordingly
+cd C:/OpenSSL-Win64/bin
+
+#Set passwords for various file types
+SET keyPassword=keypassword
+SET p12Password=p12password
+SET pemPassword=pempassword
+
+#Convert public cert from der format to pem format
+openssl.exe x509 -in %fileName%.cer -inform der -outform pem -out %fileName%.public.pem
+
+#Generate pkcs12 file (Windows)
+openssl.exe pkcs12 -export -clcerts -in %fileName%.public.pem -inkey %fileName%.key -passin pass:%keyPassword% -out %fileName%.p12 -passout pass:%p12Password%
+
+#Generate pem file with private key
+openssl.exe pkcs12 -in %fileName%.p12 -passin pass:%p12Password% -out %fileName%.private.pem -passout pass:%pemPassword% -clcerts
+
+#Generate pem file without password
+openssl.exe rsa -in %fileName%.private.pem -passin pass:%pemPassword% -out %fileName%.nopass.pem
