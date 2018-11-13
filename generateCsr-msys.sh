c61425dd8482de0f5d5ac5e4935d48e4da071da7
Paris Qian Sen <paris.qian@ncs.com.sg>
2018-11-13 17:33:56 +0800

    Add a hot fix for msys shells on Windows PC

---
+++ generateCsr-msys.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+configFile=${1}
+
+# setup the environment
+function prop {
+    grep -w "${1}" $configFile|cut -d'=' -f2
+}
+
+# Generate new PKI key pair, with key encryption - pcks1
+openssl genrsa -aes256 -out $(prop 'fileName').key -passout pass:$(prop 'keyPassword') 2048
+
+# remove passphrase from key file - pkcs1
+openssl rsa -in $(prop 'fileName').key -out $(prop 'fileName').nopass.key -passin pass:$(prop 'keyPassword')
+
+# output public key
+openssl rsa -in $(prop 'fileName').key -pubout -out $(prop 'fileName').public.key -passin pass:$(prop 'keyPassword')
+
+# convert pkcs1 to pkcs8 without passphrase
+openssl pkcs8 -topk8 -in $(prop 'fileName').nopass.key -out $(prop 'fileName').nopass.pkcs8.key -nocrypt
+
+# convert pkcs1 to pkcs8 with passphrase
+openssl pkcs8 -topk8 -in $(prop 'fileName').nopass.key -out $(prop 'fileName').pkcs8.key -passout pass:$(prop 'keyPassword')
+
+# Generate Signing Request
+# https://stackoverflow.com/a/31990313/1471046
+openssl req -new \
+    -key $(prop 'fileName').key \
+    -passin pass:$(prop 'keyPassword') \
+    -out $(prop 'fileName').csr \
+    -subj "//C=SG\ST=$(prop 'stateName')\L=$(prop 'localityName')\O=$(prop 'organizationName')\OU=$(prop 'organizationUnit')\CN=$(prop 'commonName')\emailAddress=$(prop 'email')"
